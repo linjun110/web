8df37cbaa6d24e3dba7acafbc3afb36da09e30ca
junlin <junlin@adobe.com>
2014-02-21 20:45:48 +0800

    add log4j

--- src/com/linjun/testProj/testComponent/dao/MysqlDao.java
+++ src/com/linjun/testProj/testComponent/dao/MysqlDao.java
@@ -11,6 +11,11 @@ import java.sql.Statement;
 
 
 
+
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import com.linjun.testProj.testComponent.exception.BusinessException;
 
 
@@ -19,35 +24,66 @@ public class MysqlDao implements IDao{
 	private static Statement st = null;
 	private static final MysqlDao _singleton = new MysqlDao();
 	
+	private static Logger logger;
+	
 	private MysqlDao() {
+		logger = LogManager.getLogger(MysqlDao.class);
+		connect2Db();
+	}	
+	private void connect2Db() {
+		if( st != null){
+			try {
+				logger.debug("st is not null, closing");
+				st.close();
+			} catch (SQLException e) {
+				logger.warn("st closing exception.");
+			}
+		}
+		if( con != null){
+			try {
+				logger.debug("con is not null, closing");
+				con.close();
+			} catch (SQLException e) {
+				logger.warn("con closing exception.");
+			}
+		}
 		try {
+			logger.debug("Loading mysql jdbc");
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
-			System.out.println("Load mysql jdbc failed ...");
-			e.printStackTrace();
+			logger.warn("Load mysql jdbc exception ...");
+			//e.printStackTrace();
 			return;
 		}
 		
 		try {
-			System.out.println("Connecting to db ...");
+			logger.info("Connecting to db ...");
 			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/myProject", "root", "root");
 			st = (Statement) con.createStatement();
-			System.out.println("connected to db ...");
+			logger.info("Connected to db ...");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			System.out.println("Failed to connect to db ...");
-			e.printStackTrace();
+			logger.warn("Failed to connect to db ...");
+			//e.printStackTrace();
 			return;
 		}
 	}	
-
 	public static MysqlDao getInstance() {
 		return _singleton;
 	}
 	
 	public ResultSet query(String sql) throws BusinessException {
+		logger.debug("sql: "+sql);
         if(con == null || st == null){
+        	logger.debug("con or st is null");
+        	if(con == null){
+        		logger.debug("con is null");
+        	}
+        	if(st == null){
+        		logger.debug("st is null");
+        	}
+        	connect2Db();
         	throw new BusinessException(null, "MYSQL", 1, "disconnect to mysql server");
 		}
 		try {
@@ -55,19 +91,31 @@ public class MysqlDao implements IDao{
 			return rs;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.warn("Failed to query to db, got exception.");
+			//e.printStackTrace();
+			connect2Db();
 			throw new BusinessException(e, "MYSQL", 1, e.getMessage());
 		}
 	}
 	public void execute(String sql) throws BusinessException {
+		logger.debug("sql: "+sql);
 		if(con == null || st == null){
-			throw new BusinessException(null, "MYSQL", 1, "disconnect to mysql server");
+			if(con == null){
+        		logger.debug("con is null");
+        	}
+        	if(st == null){
+        		logger.debug("st is null");
+        	}
+        	connect2Db();
+        	throw new BusinessException(null, "MYSQL", 1, "disconnect to mysql server");
 		}
 		try {
 			st.executeUpdate(sql);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//e.printStackTrace();
+			logger.warn("Failed to execute to db, got exception.");
+			connect2Db();
 			throw new BusinessException(e, "MYSQL", 1, e.getMessage());
 		}
 	}
@@ -79,11 +127,11 @@ public class MysqlDao implements IDao{
 		Statement st2 = (Statement) con2.createStatement();
 		ResultSet rs2 = st2.executeQuery("select * from user");
 		while (rs2.next()) {
-            System.out.println("first:"+rs2.getString("name")+"_"+rs2.getString("password"));
+			logger.debug("first:"+rs2.getString("name")+"_"+rs2.getString("password"));
         }  
 		ResultSet rs3 = st2.executeQuery("select * from testbo");
 		while (rs3.next()) {
-            System.out.println("second:"+rs3.getString("name")+"_"+rs3.getString("password"));
+			logger.debug("second:"+rs3.getString("name")+"_"+rs3.getString("password"));
         } 
 		st2.executeUpdate("insert into testbo (name,password) values('test','test')");
 		st2.close();
